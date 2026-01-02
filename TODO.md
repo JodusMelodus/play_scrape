99a25c44f9344b8c216c7fb5ea0d9f927d494c37
JodusMelodus <philipgricius@gmail.com>
2026-01-02 10:04:01 +0200

    better program flow

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# TODO
+
+- Input share link and extract info
--- src/main.py
+++ src/main.py
@@ -1,14 +1,39 @@
+from typing import Any
 import requests
 
-playlist_id: str|None = None
-url: str = f"https://api.deezer.com/playlist/{playlist_id}"
+url: str = "https://api.deezer.com/playlist/"
+quit: bool = False
 
-r: requests.Response = requests.get(url)
-data = r.json()
-result: str = ""
+while not quit:
+    playlist_id = ""
+    while not playlist_id:
+        playlist_id = input("Enter playlist id> ")
 
-result += str(data["title"]) + '\n'
-for track in data["tracks"]["data"]:
-    result += str(f"{track["title"]} - {track["artist"]["name"]}") + '\n'
+    print("Searching...")
 
-print(result)
+    r: requests.Response = requests.get(url+playlist_id.strip())
+    try:
+        data: dict[str, Any] = r.json()
+    except:
+        print("Failed to decode JSON")
+        continue
+
+    error = data.get("error")
+    if error:
+        print(f"""
+              An error occurred!
+              Type: {error.get("type")}
+              Message: {error.get("message")}
+              """)
+        continue
+
+    title = data.get("title", "No title")
+    tracks = data.get("tracks", {}).get("data", [])
+
+    result = f"{title}\n"
+    for track in tracks:
+        track_title = track.get("title", "No title")
+        artist_name = track.get("artist", {}).get("name", "Unknown artist")
+        result += f"{track_title} - {artist_name}\n"
+
+    print(f"------------------------------\n{result}------------------------------")
