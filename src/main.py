2d320e021879b24c3e4fbea5e1c229936eb09e59
JodusMelodus <philipgricius@gmail.com>
2026-01-02 09:03:14 +0200

    replace json export with text

--- src/main.py
+++ src/main.py
@@ -1,18 +1,14 @@
 import requests
-import json
 
 playlist_id: str|None = None
 url: str = f"https://api.deezer.com/playlist/{playlist_id}"
 
 r: requests.Response = requests.get(url)
 data = r.json()
+result: str = ""
 
-# Save data to playlist.json
-with open("playlist.json", "w", encoding="utf-8") as f:
-    string_data = str(data).replace("'", "\"").replace("True", "true").replace("False", "false")
-    json.dump(json.loads(string_data), f, indent=4)
-
-
-print(data["title"])
+result += str(data["title"]) + '\n'
 for track in data["tracks"]["data"]:
-    print(f"{track["title"]} - {track["artist"]["name"]}")
+    result += str(f"{track["title"]} - {track["artist"]["name"]}") + '\n'
+
+print(result)
